5d4a6b37eede5ed3817fc2afb3f4ffd1c3b2dc6f
tolamson2811 <100157005+tolamson2811@users.noreply.github.com>
2023-03-09 10:26:18 +0700

    sidebar

--- src/layouts/DefaultLayout/SideBar/SideBar.jsx
+++ src/layouts/DefaultLayout/SideBar/SideBar.jsx
@@ -1,10 +1,18 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
+import SidebarMenuItems from '../components/SidebarMenuItems';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
-    return <aside className={cx('wrapper')}>Sidebar</aside>;
+    return (
+        <aside className={cx('wrapper')}>
+            <div className={cx('menu-wrapper')}>
+                <h4 className={cx('menu-header')}>New Feeds</h4>
+                <SidebarMenuItems/>
+            </div>
+        </aside>
+    );
 }
 
 export default Sidebar;
