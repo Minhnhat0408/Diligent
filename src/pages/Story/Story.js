7b7960a5b41cd069456a3f7d3c48eb73628964fa
tolamson2811 <100157005+tolamson2811@users.noreply.github.com>
2023-05-08 11:47:50 +0700

    delete and auto delete story

--- src/pages/Story/Story.js
+++ src/pages/Story/Story.js
@@ -2,13 +2,14 @@ import classNames from 'classnames/bind';
 import { StoryItem } from '~/component/StoryComponents/StoryItem';
 import styles from './Story.module.scss';
 import Image from '~/component/Image';
-import React, { useContext, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import Stories from '~/component/Stories/Stories';
 import { ThemeContext } from '~/contexts/Context';
 import { UserAuth } from '~/contexts/authContext';
-import { collection, doc, getDocs, query, serverTimestamp } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, query, serverTimestamp } from 'firebase/firestore';
 import { db } from '~/firebase';
 import { useEffect } from 'react';
+import getTimeDiff from '~/utils/timeDiff';
 const cx = classNames.bind(styles);
 
 function Story() {
@@ -19,60 +20,16 @@ function Story() {
     const [myStories, setMyStories] = useState(null);
     const [hasFetchedStories, setHasFetchedStories] = useState(false);
 
-    useEffect(() => {
-        async function fetchStories() {
-            const q = query(collection(db, 'stories'));
-            const querySnapshot = await getDocs(q);
-            const docs = querySnapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            setStoriesData(docs);
-            const story = docs
-                .filter((story) => story.user.id === user.uid)
-                .map((story) => ({
-                    url: story.media[0],
-                    username: story.user.name,
-                    time: story.time,
-                    avatar: story.user.avatar,
-                    posX: story.content.posX,
-                    posY: story.content.posY,
-                    paragraph: story.content.text,
-                    bgColor: story.content.bgColor,
-                    textColor: story.content.textColor,
-                    text: story.content.text,
-                    scale: story.scale,
-                    duration: 3000,
-                    userId: story.user.id,
-                    storyId: story.id,
-                }));
-
-            const sortedStory = story.sort((a, b) => a.time.seconds - b.time.seconds);
-
-            setMyStories(sortedStory);
-            setHasFetchedStories(true);
-        }
-        fetchStories();
-    }, [hasFetchedStories]);
-
-    const storyByUserId = Object.values(
-        storiesData.reduce((acc, story) => {
-            const test = {
-                userId: story.user.id,
-                username: story.user.name,
-                avatar: story.user.avatar,
-            };
-            if (!acc[test.userId]) {
-                acc[test.userId] = test;
-            }
-
-            return acc;
-        }, {}),
-    );
-
-    async function selectStory(id) {
-        const story = storiesData
-            .filter((story) => story.user.id === id)
+    async function fetchStories() {
+        const q = query(collection(db, 'stories'));
+        const querySnapshot = await getDocs(q);
+        const docs = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        setStoriesData(docs);
+        const story = docs
+            .filter((story) => story.user.id === user.uid)
             .map((story) => ({
                 url: story.media[0],
                 username: story.user.name,
@@ -89,12 +46,72 @@ function Story() {
                 userId: story.user.id,
                 storyId: story.id,
             }));
+
         const sortedStory = story.sort((a, b) => a.time.seconds - b.time.seconds);
 
         setMyStories(sortedStory);
+        setHasFetchedStories(true);
     }
 
-    console.log(myStories == null);
+    useEffect(() => {
+        fetchStories();
+        autoDelete();
+    }, [hasFetchedStories]);
+
+    const storyByUserId = Object.values(
+        storiesData.reduce((acc, story) => {
+            const val = {
+                userId: story.user.id,
+                username: story.user.name,
+                avatar: story.user.avatar,
+            };
+            if (!acc[val.userId]) {
+                acc[val.userId] = val;
+            }
+
+            return acc;
+        }, {}),
+    );
+
+    async function selectStory(id) {
+        const selectedStories = storiesData.filter((story) => story.user.id === id);
+        const sortedStory = selectedStories.sort((a, b) => a.time.seconds - b.time.seconds);
+
+        const formattedStories = sortedStory.map((story) => ({
+            url: story.media[0],
+            username: story.user.name,
+            time: story.time,
+            avatar: story.user.avatar,
+            posX: story.content.posX,
+            posY: story.content.posY,
+            paragraph: story.content.text,
+            bgColor: story.content.bgColor,
+            textColor: story.content.textColor,
+            text: story.content.text,
+            scale: story.scale,
+            duration: 3000,
+            userId: story.user.id,
+            storyId: story.id,
+        }));
+
+        setMyStories(formattedStories);
+    }
+
+    // Xóa story đã đăng quá 1 ngày
+    async function autoDelete() {
+        storiesData.map((story) => {
+            const time = getTimeDiff(Date.now(), story.time.seconds * 1000);
+            if (time.includes('days')) {
+                deleteDoc(doc(db, 'stories', story.id));
+            }
+        });
+    }
+
+    const handleDeleteStory = useCallback(() => {
+        fetchStories();
+    }, []);
+
+    // console.log(storiesData);
 
     return (
         <div className={cx('wrapper', { dark: context.theme === 'dark' })}>
@@ -122,7 +139,11 @@ function Story() {
                 {/* phần hiển thị story  */}
                 {myStories != null && myStories[0] != undefined ? (
                     <>
-                        <Stories stories={myStories} key={JSON.stringify(myStories)} />
+                        <Stories
+                            stories={myStories}
+                            key={JSON.stringify(myStories)}
+                            onDeleteStory={handleDeleteStory}
+                        />
                         <div className={cx('react')}>
                             <textarea className={cx('reply')} placeholder="Reply..."></textarea>
                             <div className={cx('action')}>
