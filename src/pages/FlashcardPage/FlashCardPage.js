df8caf2944c72d4766b7c2ac568e90a8c9ea4c94
qbert000 <quyen1412kid@gmail.com>
2023-05-12 10:01:15 +0700

    fix

--- src/pages/FlashcardPage/FlashCardPage.js
+++ src/pages/FlashcardPage/FlashCardPage.js
@@ -1,75 +1,39 @@
-import classNames from 'classnames/bind'
-import styles from './FlashCardPage.module.scss'
+import classNames from 'classnames/bind';
+import styles from './FlashCardPage.module.scss';
 import { useContext } from 'react';
 import { ThemeContext } from '~/contexts/Context';
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function FlashCardPage() {
-    const context = useContext(ThemeContext)
-    return ( <div className={cx('wrapper',{dark: context.theme ===  'dark'})}>
-        <div className={cx('user-decks')}>
-            <div className={cx('deck')}>
-                
+    const context = useContext(ThemeContext);
+    return (
+        <div className={cx('wrapper', { dark: context.theme === 'dark' })}>
+            <div className={cx('user-decks')}>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
+                <div className={cx('deck1')}></div>
             </div>
-            <div className={cx('deck')}>
-
+            <div className={cx('all')}>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
+                <div className={cx('deck2')}></div>
             </div>
-            <div className={cx('deck')}>
-
-            </div>
-            <div className={cx('deck')}>
-
-            </div>
-            <div className={cx('deck')}>
-
-            </div>
-            <div className={cx('deck')}>
-
-            </div>
-            <div className={cx('deck')}>
-
-            </div>
-            <div className={cx('deck')}>
-
-            </div>
-            <div className={cx('deck')}>
-
-            </div>
-           
         </div>
-        <div className={cx('all')}>
-    
-            <div className={cx('deck')}>
-            </div>
-            <div className={cx('deck')}>
-
-</div>
-<div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div><div className={cx('deck')}>
-
-</div>
-
-       
-
-
-        </div>
-         
-    </div> );
+    );
 }
 
-export default FlashCardPage;       
+export default FlashCardPage;
