0f1489a29e2d7870482b8dc41515f84c958ec059
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-05-22 15:01:13 +0700

    responsive header (not for < 670px) screen

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,9 +5,10 @@ const image = {
     noImage: require('~/assets/images/download.jpg'),
     loginImg: require('~/assets/images/loginImage.png'),
     google: require('~/assets/images/google.png'),
-    social:require('~/assets/images/socialmedia.jpg'),
-    baduc:require('~/assets/images/baduc.jpg'),
-    noContent: require('~/assets/images/no_content.png')
-  };
-  
-  export default image;
+    social: require('~/assets/images/socialmedia.jpg'),
+    baduc: require('~/assets/images/baduc.jpg'),
+    noContent: require('~/assets/images/no_content.png'),
+    noText: require('~/assets/images/favicon.ico.png'),
+};
+
+export default image;
