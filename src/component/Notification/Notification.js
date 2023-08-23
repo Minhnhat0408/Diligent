25579fcb78108465dd725373028bdeab966e9759
MinhMatMong <kurominhnhat@gmail.com>
2023-08-23 11:25:08 +0700

    fix res again

--- src/component/Notification/Notification.js
+++ src/component/Notification/Notification.js
@@ -43,16 +43,16 @@ function Notification({ data, time, ...props }) {
     return (
         <Link to={data.url} {...props} className={cx('wrapper', { dark: context.theme === 'dark' })}>
             <div className={cx('img')}>
-                <Image src={data.sender.avatar} className={cx('avatar')} alt="avatar" />
-                <FontAwesomeIcon icon={icon.current} className={cx('icon')} />
+                <Image src={data.sender.avatar} className={cx('avatar') + " 2xl-max:!w-10 2xl-max:!h-10"} alt="avatar" />
+                <FontAwesomeIcon icon={icon.current} className={cx('icon') + " 2xl-max:!w-3 + 2xl-max:!h-3 2xl-max:!p-1 2xl-max:!text-[10px] "} />
             </div>
             <div className={cx('info')}>
-                <h4 className={cx('content')}>
-                    <span className={cx('name')}>{data.sender.name}</span> {data.title}
+                <h4 className={cx('content') + " 2xl-max:!text-[14px]"}>
+                    <span className={cx('name') }>{data.sender.name}</span> {data.title}
                 </h4>
-                <span className={cx('time')}>{time}</span>
+                <span className={cx('time') + " 2xl-max:!text-[12px]"}>{time}</span>
             </div>
-            <span className={cx('status', { read: data.read })}>
+            <span className={cx('status', { read: data.read }) + " 2xl-max:!text-[8px]"}>
                 <FontAwesomeIcon icon={faCircle} />
             </span>
         </Link>
